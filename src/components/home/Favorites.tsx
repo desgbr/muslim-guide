f941f842a059bb05fec0839eff6082a5df86a053
Ahmed gabr <amoustafa@v4tech.com>
2024-10-26 19:24:37 +0300

    handled empty favorite

--- src/components/home/Favorites.tsx
+++ src/components/home/Favorites.tsx
@@ -8,6 +8,7 @@ import { Title } from "@/types/azkar";
 function FavoritesList() {
   const { count, favorite } = useFavorite();
   console.log("count, favorite", count, favorite);
+
   return (
     <div className="col-span-full">
       <div className="flex gap-2 items-center mb-4">
@@ -18,9 +19,15 @@ function FavoritesList() {
         </div>
       </div>
       <div className="grid md:grid-cols-3 gap-4">
-        {favorite.map((item: Title) => (
-          <FavoriteCard key={`favorite-card-${item.id}`} item={item} />
-        ))}
+        {favorite.length === 0 ? (
+          <div className="col-span-full text-slate-300 text-xl md:text-2xl flex items-center justify-center rounded-md border p-4 md:p-8">
+            لا يوجد عناصر في المفضلة
+          </div>
+        ) : (
+          favorite.map((item: Title) => (
+            <FavoriteCard key={`favorite-card-${item.id}`} item={item} />
+          ))
+        )}
       </div>
     </div>
   );
