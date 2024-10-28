39fa0ce1b96334f3f77c2e7a5cf572bb1f8c578d
Ahmed gabr <amoustafa@v4tech.com>
2024-10-28 08:43:45 +0300

    fixed mixed content issue

--- next.config.ts
+++ next.config.ts
@@ -2,20 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  async headers() {
-    return [
-      {
-        // Apply to all routes
-        source: '/:path*',
-        headers: [
-          {
-            key: 'Content-Security-Policy',
-            value: 'upgrade-insecure-requests',
-          },
-        ],
-      },
-    ];
-  },
 };
 
 export default nextConfig;
