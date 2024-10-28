19e1258403ab90ab8ee7ea0a1dffdf34cfa3a33f
Ahmed gabr <amoustafa@v4tech.com>
2024-10-28 08:20:37 +0300

    fixed mixed content issue

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,20 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async headers() {
+    return [
+      {
+        // Apply to all routes
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: 'upgrade-insecure-requests',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Cairo } from "next/font/google";
 import "./globals.css";
 import Layout from "@/layout";
-import Head from "next/head";
 
 const zain = Cairo({
   weight: ["400", "300", "700", "200", "300"],
@@ -15,6 +14,9 @@ export const metadata: Metadata = {
   title: "موقع دليل المسلم",
   description:
     "موقع يحتوي علي كل ما يهم المسلم في يوممه وليلته من الاذكار الواردة في الكتاب والسنة",
+  other:{
+
+  }
 };
 
 export default function RootLayout({
@@ -24,9 +26,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ar" dir="rtl" className={`${zain.variable}`}>
-    <Head>
-      <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"/>
-    </Head>
     <body>
     <Layout>{children}</Layout>
     </body>
